fa07b0eedc7f15109a1ec3df4c7ab21967929ea3
Jamie Doris <jamiedoris@Jamies-MacBook-Pro.local>
2025-07-09 18:07:03 -0400

    neogit & tmux conf, lualine theme fix

--- .config/nvim/lua/custom/plugins/neogit.lua
+++ .config/nvim/lua/custom/plugins/neogit.lua
@@ -5,6 +5,10 @@ return {
     'sindrets/diffview.nvim', -- optional - Diff integration
     'nvim-telescope/telescope.nvim', -- optional
   },
-  vim.keymap.set('n', '<leader>gs', ':Neogit kind=vsplit<CR>', { desc = 'Neo[G]it V[S]plit' }),
+  vim.keymap.set('n', '<leader>gv', ':Neogit kind=vsplit<CR>', { desc = 'Neo[G]it [V]split' }),
   vim.keymap.set('n', '<leader>gf', ':Neogit kind=floating<CR>', { desc = 'Neo[G]it [F]loating' }),
+  vim.keymap.set('n', '<leader>gs', ':Neogit stash<CR>', { desc = 'Neo[G]it [S]tash' }),
+  vim.keymap.set('n', '<leader>gd', ':Neogit diff<CR>', { desc = 'Neo[G]it [D]iff' }),
+  vim.keymap.set('n', '<leader>g<', ':Neogit pull<CR>', { desc = 'Neo[G]it [<]pull' }),
+  vim.keymap.set('n', '<leader>g>', ':Neogit push<CR>', { desc = 'Neo[G]it [>]push' }),
 }
